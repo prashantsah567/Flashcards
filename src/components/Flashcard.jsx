0e861710e9cf5dd47f13e9ae227e58989e1bf4e9
Prashant Sah <prashantsah567@gmail.com>
2023-03-05 16:47:44 -0500

    questions and answers added

--- src/components/Flashcard.jsx
+++ src/components/Flashcard.jsx
@@ -2,16 +2,16 @@ import React, {useState} from "react";
 import "./FlashCard.css";
 
 const flashcardsData = [
-    {question: "1what is jsx?", answer: "1JavaScript Syntax Extension"},
-    {question: "2what is jsx?", answer: "2JavaScript Syntax Extension"},
-    {question: "3what is jsx?", answer: "3JavaScript Syntax Extension"},
-    {question: "4what is jsx?", answer: "4JavaScript Syntax Extension"},
-    {question: "5what is jsx?", answer: "5JavaScript Syntax Extension"},
-    {question: "6what is jsx?", answer: "6JavaScript Syntax Extension"},
-    {question: "7what is jsx?", answer: "7JavaScript Syntax Extension"},
-    {question: "8what is jsx?", answer: "8JavaScript Syntax Extension"},
-    {question: "9what is jsx?", answer: "9JavaScript Syntax Extension"},
-    {question: "10what is jsx?", answer: "10JavaScript Syntax Extension"},
+    {question: "What does DOM stands for?", answer: "Document Object Model"},
+    {question: "What is a closure?", answer: "It's an inner function that has access to the outer function's variables and parameters"},
+    {question: "What does JSX stands for?", answer: "JavaScript Syntax Extension"},
+    {question: "What is the virtual DOM in React?", answer: "It's a lightweight representation of the actual DOM used by React for performance optimization"},
+    {question: "What is purpose of the 'useEffect' hook in React?", answer: "It is used to perform side effects in a React functional component, such as fetching data or updating the DOM"},
+    {question: "What is the difference between 'let' and 'const' in JS?", answer: "'Let' declares a variable that can be reassigned, while 'const' declares a variable that cannot be reassigned"},
+    {question: "What is the purpose of the 'map' function in JS?", answer: "It is used to iterate over an array and transform its element into a new array"},
+    {question: "What is the difference between 'null' and 'undefined'?", answer: "'null' represents the intentional absence of any object value, while 'undefined' represents the absence of a value"},
+    {question: "What is the purpose of 'reduce' function in JS?", answer: "It is used to iterate over an array and accumlate its value into a single value"},
+    {question: "What is the purpose fo the 'this' keyword in JS?", answer: "The 'this' keyword refers to the object that the function is a method of, or the global if the function is not a method of any object"},
 ];
 
 const FlashCard = () => {
@@ -38,7 +38,7 @@ const FlashCard = () => {
         </div>
         <br/>
         <div>
-        <button onClick={handleForwardClick}>→</button>
+        <button onClick={handleForwardClick} className="btn">→</button>
         </div>
         </>
     )
